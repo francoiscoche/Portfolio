39dc36281063ceefe9b1bf7ae4ae07486a4355c3
Francois <francois.coche@outlook.it>
2022-09-02 08:52:19 +0200

    rebuild page "exp" vuejs friendly

--- src/views/Exp.vue
+++ src/views/Exp.vue
@@ -9,7 +9,38 @@
                 <img alt="Vue logo" src="../assets/TMA.png">
             </div> -->
                 <!-- <div class="row"> -->
-                <div class="card mb-7" style="">
+
+                <div
+                    v-for="(datas, index) in card_infos"
+                    v-bind:key="index"
+                    class="card-container"
+                >
+                    <div class="img-card">
+                        <img
+                            class="logo-card"
+                            :src="datas.img"
+                            alt="img card"
+                        />
+                    </div>
+                    <div class="text-card">
+                        <h5 class="title-card">
+                            {{ datas.title }}
+                        </h5>
+                        <div class="text-mission-card">
+                            {{ datas.text }}
+                        </div>
+                        <div class="etiquette-mission-card">
+                            <div
+                                v-for="(label, index) in datas.labels"
+                                v-bind:key="index"
+                            >
+                                <div>{{ label }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="card mb-7" style="">
                     <div class="row no-gutters">
                         <div class="col-md-4">
                             <img
@@ -77,7 +108,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <!-- </div> -->
             </div>
         </section>
@@ -87,11 +118,112 @@
 <script>
 export default {
     name: 'Exp',
+    data() {
+        return {
+            card_infos: [
+                {
+                    title: 'Side Project - Track My Assets',
+                    text: 'Track My Asset is a tracker for NFT collections. It allows you to receive personalized Discord, Telegram or Mail notifications when the collection you have been following has changed its floor price.',
+                    img: require('@/assets/TMA.png'),
+                    labels: {
+                        0: 'Web3',
+                        1: 'NodeJs',
+                        2: 'Sass',
+                        3: 'Discord Bot',
+                        4: 'Telegram Bot',
+                        5: 'Web APIs',
+                    },
+                },
+                {
+                    title: 'Sopra Steria - Consultant',
+                    text: 'This is a wider card with supporting textbelow as a natural lead-in to additional content. This content is a little bit longer his is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+                    img: require('@/assets/img/altice.png'),
+                    labels: {
+                        0: 'Web3',
+                        1: 'NodeJs',
+                        2: 'Sass',
+                        3: 'Discord Bot',
+                        4: 'Telegram Bot',
+                        5: 'Web APIs',
+                    },
+                },
+                {
+                    title: 'Sopra Steria - Consultant',
+                    text: 'This is a wider card with supporting textbelow as a natural lead-in to additional content. This content is a little bit longer his is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+                    img: require('@/assets/img/justice.png'),
+                    labels: {
+                        0: 'Web3',
+                        1: 'NodeJs',
+                        2: 'Sass',
+                        3: 'Discord Bot',
+                        4: 'Telegram Bot',
+                        5: 'Web APIs',
+                    },
+                },
+                {
+                    title: 'Sopra Steria - Consultant',
+                    text: 'This is a wider card with supporting textbelow as a natural lead-in to additional content. This content is a little bit longer his is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+                    img: require('@/assets/img/altice.png'),
+                    labels: {
+                        0: 'Web3',
+                        1: 'NodeJs',
+                        2: 'Sass',
+                        3: 'Discord Bot',
+                        4: 'Telegram Bot',
+                        5: 'Web APIs',
+                    },
+                },
+                {
+                    title: 'Sopra Steria - Consultant',
+                    text: 'This is a wider card with supporting textbelow as a natural lead-in to additional content. This content is a little bit longer his is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+                    img: require('@/assets/img/fdj.png'),
+                    labels: {
+                        0: 'Web3',
+                        1: 'NodeJs',
+                        2: 'Sass',
+                        3: 'Discord Bot',
+                        4: 'Telegram Bot',
+                        5: 'Web APIs',
+                    },
+                },
+            ],
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 @import './../sass/variables';
+
+.card-container {
+    background-color: black;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid white;
+    max-width: 1000px;
+    padding: 30px;
+
+    .img-card {
+        .logo-card {
+            width: 200px; // a modifier
+            // margin: 30px;
+        }
+    }
+    .text-card {
+        padding-left: 20px;
+        .etiquette-mission-card {
+            display: flex;
+            flex-direction: row;
+
+            div {
+                background-color: white;
+                color: black;
+                margin: 6px;
+            }
+        }
+    }
+}
 
 section {
     margin: 30;
